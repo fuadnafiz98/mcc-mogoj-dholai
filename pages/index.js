b32a965dbf47b9e87a0a2ce21763e9d5f7e7920c
fuadnafiz98 <fuadnafiz98@gmail.com>
2021-05-10 02:55:49 +0600

    metatag added

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,26 @@ import Image from "next/image";
 
 export default function Index() {
   return (
-    <div className="h-screen p-6 bg-yellow-100 lg:p-12 lg:grid lg:place-content-center">
-      <div className="grid place-content-center">
-        <Image src="/event.jpg" width={1200} height={600} />
+    <div>
+      <Head>
+        <title>Mogoj Dholai</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <meta property="og:title" content="Mogoj Dholai" />
+        <meta
+          property="og:description"
+          content="Mogoj Dholai Organized By MCC"
+        />
+        <meta property="og:image" content="/event.jpg" />
+        <meta property="og:url" content="https://mcc.vercel.app" />
+
+        <meta property="og:site_name" content="Mogoj Dholai" />
+        <meta name="twitter:image:alt" content="Mogoj Dholai Event" />
+      </Head>
+      <div className="h-screen p-6 bg-yellow-100 lg:p-12 lg:grid lg:place-content-center">
+        <div className="grid place-content-center">
+          <Image src="/event.jpg" width={1200} height={600} />
+        </div>
       </div>
     </div>
   );
